e2e9e25772a9ddf19d788fdae9342c84dac898af
Diego Jiménez <diego.o.jimz@gmail.com>
2026-08-12 10:29:42 -0600

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9 ; i++) {
+            int v1 = 0, v2 = 0, v3 = 0, val;
+            char c;
+
+            for (int j = 0; j < 9 ; j++) {
+                // rows
+                c = board[i][j];
+                if (c != '.') {
+                    val = c - '0' - 1;
+                    if (v1 & (1 << val)) return false;
+                    v1 |= (1 << val);
+                }
+
+                // cols
+                c = board[j][i];
+                if (c != '.') {
+                    val = c - '0' - 1;
+                    if (v2 & (1 << val)) return false;
+                    v2 |= (1 << val);
+                }
+
+                // rows
+                c = board[(3 * (i / 3)) + (j / 3)][(3 * (i % 3)) + (j % 3)];
+                if (c != '.') {
+                    val = c - '0' - 1;
+                    if (v3 & (1 << val)) return false;
+                    v3 |= (1 << val);
+                }
+            }
+        }
+        return true;
+    }
+};
