9f6368a2a7f7dc6001a078e07230984d612fd9ad
Diego Jiménez <diego.o.jimz@gmail.com>
2026-09-21 11:30:49 -0600

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.cpp
@@ -0,0 +1,24 @@
+/*
+    a, ab, abc
+    d, cd, bcd
+*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        size_t s = nums.size();
+        vector<int> prefix(s, 1), suffix(s, 1);
+
+        for (int i = 1; i < s; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+            suffix[i] = suffix[i - 1] * nums[s - i];
+        }
+
+        vector<int> res;
+        for (int i = 0; i < s; i++) {
+            res.push_back(prefix[i] * suffix[s - i - 1]);
+        }
+
+        return res;
+    }
+};
