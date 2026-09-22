c52fb717097c86d8cb14f083a372c2cedf62b690
Diego Jiménez <diego.o.jimz@gmail.com>
2026-09-22 14:58:01 -0600

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> stk;
+        vector<int> res(temperatures.size(), 0);
+
+        stk.push(0);
+
+        for (int i = 1; i < temperatures.size(); ++i) { 
+            while (!stk.empty() && temperatures[stk.top()] < temperatures[i]) {
+                res[stk.top()] = i - stk.top();
+                stk.pop();
+            } 
+            stk.push(i);
+        }
+        
+        return res;
+    }
+};
